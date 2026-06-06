431d3fdba52e6e8b2956b4b1a5058c3b008bb945
Jaqueline-Diana <jaqe.0104@gmail.com>
2026-06-05 23:19:50 -0600

    Fix host para Railway

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from database import get_connection, inicializar_db
 
@@ -141,4 +142,4 @@ def ver_expediente(personal_id):
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
