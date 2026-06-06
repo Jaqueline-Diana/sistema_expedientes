de0c41f6f3757e2f3cf2c065e5b1d3c13ece920d
Jaqueline-Diana <jaqe.0104@gmail.com>
2026-06-05 23:23:57 -0600

    Fix puerto 8080

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ def ver_expediente(personal_id):
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
