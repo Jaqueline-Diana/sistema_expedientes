230971f3f51061f922be6e5129b73385f3dcae2a
Jaqueline-Diana <jaqe.0104@gmail.com>
2026-06-05 23:28:04 -0600

    Fix PORT variable

--- app.py
+++ app.py
@@ -142,4 +142,5 @@ def ver_expediente(personal_id):
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
